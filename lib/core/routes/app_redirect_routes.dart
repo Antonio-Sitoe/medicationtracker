91d8e20a648fe2eb3a148f094de72019b30a6355
Antonio Sitoe <antoniositoehl@gmail.com>
2025-06-09 08:33:27 +0200

    Update app_redirect_routes.dart

--- lib/core/routes/app_redirect_routes.dart
+++ lib/core/routes/app_redirect_routes.dart
@@ -23,7 +23,7 @@ String? appRedirectRoutes(GoRouterState state, AuthViewModel auth) {
   }
 
   if (auth.isAuthenticated && publicRoutes.contains(currentRoute)) {
-    return AppNamedRoutes.patientTabsMedications;
+    return AppNamedRoutes.patientTabsHome;
   }
 
   return null;
